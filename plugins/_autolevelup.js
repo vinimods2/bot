9185c0ba23e62807cfe5bd0b10e6614fa8559df7
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-08 19:05:21 -0300

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -6,21 +6,21 @@ handler.before = m => {
 	if (!user.autolevelup) return !0
 	let before = user.level * 1
 	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-        let role = ((user.level >= 1) && (user.level <= 10)) ? 'Beginner'
-          : ((user.level >= 10) && (user.level <= 20)) ? 'Kittel Town'
-          : ((user.level >= 20) && (user.level <= 30)) ? 'Black Woods'
-          : ((user.level >= 30) && (user.level <= 40)) ? 'Farmount'
-          : ((user.level >= 40) && (user.level <= 50)) ? 'Rosenvale'
-          : ((user.level >= 50) && (user.level <= 60)) ? 'Amberhill'
-          : ((user.level >= 60) && (user.level <= 70)) ? 'Master Fap'
-          : ((user.level >= 70) && (user.level <= 80)) ? 'Master Woods'
-          : 'Legend'
+        let role = ((user.level >= 1) && (user.level <= 10)) ? 'Bronze'
+          : ((user.level >= 10) && (user.level <= 20)) ? 'Prata'
+          : ((user.level >= 20) && (user.level <= 30)) ? 'Ouro'
+          : ((user.level >= 30) && (user.level <= 40)) ? 'Platina'
+          : ((user.level >= 40) && (user.level <= 50)) ? 'Diamante'
+          : ((user.level >= 50) && (user.level <= 60)) ? 'Mestre'
+          : ((user.level >= 60) && (user.level <= 70)) ? 'Desafiante'
+          : ((user.level >= 70) && (user.level <= 80)) ? 'Elite'
+          : 'Elite Pro'
 
 	if (before !== user.level) {
             m.reply(`
-Selamat, anda telah naik level!
+*Parabéns, você subiu de nível!*
 *${before}* -> *${user.level}*
-gunakan *.profile* untuk mengecek
+*Usar* *.profile* *checar*
 	`.trim())
             user.role = role
         }

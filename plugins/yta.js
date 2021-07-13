f30ae7ab56b23928d639d2f53aebda40a108babd
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-13 13:04:12 -0300

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
 `.trim(), m)
 }
 handler.help = ['mp3','a'].map(v => 'yt' + v + ' 〘url〙')
-handler.tags = ['musica']
+handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
 handler.owner = false
 handler.mods = false

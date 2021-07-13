d339223388134850e645b71d56b1ca09ee3f1240
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-13 12:09:48 -0300

    Update math_answer.js

--- plugins/math_answer.js
+++ plugins/math_answer.js
@@ -1,26 +1,26 @@
-global.math = global.math ? global.math : {}
-let handler = async (m, { conn }) => {
+let handler = m => m
+handler.before = async function (m) {
+  if (!/^-?[0-9]+(\.[0-9]+)?$/.test(m.text)) return !0
   let id = m.chat
-  if (!m.quoted || m.quoted.sender != conn.user.jid || !/^Quantos resultados de/i.test(m.quoted.text)) throw false
-  if (!(id in global.math)) throw 'Essa pergunta acabou'
-  if (m.quoted.id == global.math[id][0].id) {
-  let math = global.math[id][1]
-  if (m.text == math.result) {
-    conn.reply(m.chat, `*Resposta correta!*\n+${math.bonus} XP`, m)
-    global.DATABASE._data.users[m.sender].exp += math.bonus
-    clearTimeout(global.math[id][3])
-    delete global.math[id]
-  } else {
-    if (--global.math[id][2] == 0) {
-      conn.reply(m.chat, `*A oportunidade acabou!*\nResponda: *${math.result}*`, m)
-      clearTimeout(global.math[id][3])
-      delete global.math[id]
-    } else conn.reply(m.chat, `*Resposta errada!*\nAinda disponível ${global.math[id][2]} oportunidade`, m)
+  if (!m.quoted || m.quoted.sender != this.user.jid || !/^*Qual é o resultado de*/i.test(m.quoted.text)) return !0
+  this.math = this.math ? this.math : {}
+  if (!(id in this.math)) return m.reply('*Esse assunto acabou*')
+  if (m.quoted.id == this.math[id][0].id) {
+    let math = JSON.parse(JSON.stringify(this.math[id][1]))
+    if (m.text == math.result) {
+      global.db.data.users[m.sender].exp += math.bonus
+      clearTimeout(this.math[id][3])
+      delete this.math[id]
+      m.reply(`*Resposta correta Parabéns!*\n+${math.bonus} XP`)
+    } else {
+      if (--this.math[id][2] == 0) {
+        clearTimeout(this.math[id][3])
+        delete this.math[id]
+        m.reply(`*A oportunidade está se esgotando!*\n*Responder*: *${math.result}*`)
+      } else m.reply(`*Resposta errada!*\n*Ainda disponivel* ${this.math[id][2]} *oportunidade*»)
+    }
   }
- }
+  return !0
 }
-handler.customPrefix = /^-?[0-9]+(\.[0-9]+)?$/
-handler.command = new RegExp
-handler.exp = 0
 
 module.exports = handler

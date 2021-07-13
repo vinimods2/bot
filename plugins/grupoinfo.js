0c680a1cb0c54ebd628837d3e2ba4a4fb796138e
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-13 13:21:14 -0300

    Update grupoinfo.js

--- plugins/grupoinfo.js
+++ plugins/grupoinfo.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
-  let txt = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v =>`${conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'Left' : 'Joined'}]`).join`\n\n`
-  conn.reply(m.chat, 'List Groups:\n' + txt, m)
+  let txt = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v =>`${conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'Saiu' : 'Entrou'}]`).join`\n\n`
+  conn.reply(m.chat, 'Grupos de lista:\n' + txt, m)
 }
 handler.help = ['grupoinfo', 'grouplist']
 handler.tags = ['info']

27f96c7a0e5c6cd164df05d174f2da48786cc116
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-13 12:23:26 -0300

    Update doar.js

--- plugins/doar.js
+++ plugins/doar.js
@@ -1,6 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 DOAÇAO 」
 │ • ＶＩＮＩＭＯＤＳ [https://wa.me/5514997239463]
+│ • Pix henriquevinicius511@gmail.com [NUBANK]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['doar']

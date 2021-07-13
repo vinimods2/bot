cdbdf3989a3b6971b76c2b40385fb8b1be5a80fb
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-13 07:42:21 -0300

    Update adicionartest.js

--- plugins/adicionartest.js
+++ plugins/adicionartest.js
@@ -1,11 +1,38 @@
-let handler = async (m, { conn, text }) => {
-  let users = text.split`,`.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
-  conn.groupAdd(m.chat, users)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text, participants }) => {
+  let _participants = participants.map(user => user.jid)
+  let users = (await Promise.all(
+    text.split(',')
+      .map(v => v.replace(/[^0-9]/g, ''))
+      .filter(v => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
+      .map(async v => [
+        v,
+        await conn.isOnWhatsApp(v + '@s.whatsapp.net')
+      ])
+  )).filter(v => v[1]).map(v => v[0] + '@c.us')
+  let response = await conn.groupAdd(m.chat, users)
+  let pp = await conn.getProfilePicture(m.chat).catch(_ => false)
+  let jpegThumbnail = pp ? await (await fetch(pp)).buffer() : false
+  for (let user of response.participants.filter(user => Object.values(user)[0].code == 403)) {
+    let [[jid, {
+      invite_code,
+      invite_code_exp
+    }]] = Object.entries(user)
+    let teks = `*Convidativo* @${jid.split('@')[0]} *usando convite*... `
+    m.reply(teks, null, {
+      contextInfo: {
+        mentionedJid: conn.parseMention(teks)
+      }
+    })
+    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, '*Convite para aderir ao meu grupo WhatsApp*', jpegThumbnail ? {
+      jpegThumbnail
+    } : {})
+  }
 }
-handler.help = ['adicionar', '+'].map(v => 'o' + v + ' @user')
+handler.help = ['add', '+'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
-handler.command = /^(adicionar|o\+)$/i
-handler.owner = false
+handler.command = /^(oadd|o\+)$/i
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = true

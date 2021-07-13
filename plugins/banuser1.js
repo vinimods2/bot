3aa78610ed34aec3c741e1c5a4ae308e7c857ab9
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-13 11:51:55 -0300

    Update banuser1.js

--- plugins/banuser1.js
+++ plugins/banuser1.js
@@ -1,25 +1,16 @@
-let { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, text}) => {
-    if (!text) throw 'Quem voce quer banir?'
+let handler = async (m, { conn, text }) => {
+    if (!text) throw 'Quem quer ser banido?'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Pronto banido'
-    let users = global.DATABASE._data.users
+    if (!who) throw 'Tag um'
+    let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `berhasil banned`, m)
+    conn.reply(m.chat, `banido com sucesso!`, m)
 }
-handler.help = ['ban 〘@user〙']
-handler.tags = ['dono']
+handler.help = ['ban']
+handler.tags = ['owner']
 handler.command = /^ban$/i
 handler.rowner = true
-handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = true
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
 
 module.exports = handler

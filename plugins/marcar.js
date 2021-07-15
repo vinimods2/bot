1313505ea87dec6c9ec1813b18ef60f70640be07
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-15 00:04:16 -0300

    Update marcar.js

--- plugins/marcar.js
+++ plugins/marcar.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+  m.reply(text + '\n╾' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n╼`, null, {
     contextInfo: { mentionedJid: users }
   })
 }

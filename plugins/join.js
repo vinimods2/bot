ef0658b498d4694f74b637a3292801f417688beb
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2021-07-15 00:17:12 -0300

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,12 +1,12 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+let linkRegex = /https://chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text }) => {
     let [_, code] = text.match(linkRegex) || []
-    if (!code) throw 'Link invalid'
+    if (!code) throw 'Link inválido'
     let res = await conn.acceptInvite(code)
-    m.reply(`Berhasil join grup ${res.gid}`)
+    m.reply(`Juntou-se ao grupo com sucesso ${res.gid}`)
 }
-handler.help = ['join <chat.whatsapp.com>']
+handler.help = ['gpprem <link>']
 handler.tags = ['premium']
 
 handler.command = /^join$/i
